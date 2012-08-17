9b8ab0b5d347ca619b0836388f751aefd7f9fdc9
Stefan Hepp <stefan@stefant.org>
2012-08-17 17:30:04 +0200

    added custom machine header files for patmos to define machinedependent macros

---
+++ newlib/libc/machine/patmos/machine/param.h
@@ -0,0 +1,9 @@
+#ifndef _MACHPARAM_H
+#define _MACHPARAM_H
+
+
+#define PATH_MAX 1024
+#define MAXPATHLEN PATH_MAX
+
+
+#endif /* _MACHPARAM_H */
---
+++ newlib/libc/machine/patmos/machine/time.h
@@ -0,0 +1,8 @@
+#ifndef	_MACHTIME_H_
+#define	_MACHTIME_H_
+
+#define _CLOCKS_PER_SEC_ 100000000
+
+#endif	/* _MACHTIME_H_ */
+
+
