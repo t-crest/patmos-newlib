a4026d1a5ddaa73c388d2e0d7811dc5a4b40474f
Stefan Hepp <stefan@stefant.org>
2012-10-10 12:35:35 +0200

    added ftruncate stub

---
+++ libgloss/patmos/ftruncate.c
@@ -0,0 +1,33 @@
+// Copyright 2012 Stefan Hepp
+// 
+// This file is part of the newlib C library for the Patmos processor.
+// 
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// (COPYING3.LIB). If not, see <http://www.gnu.org/licenses/>.
+
+#include "patmos.h"
+#include <errno.h>
+#include <unistd.h>
+
+#undef errno
+extern int  errno;
+
+//******************************************************************************
+/// ftruncate - truncate a file
+int ftruncate (int file, off_t length)
+{
+    // TODO implement for simulator target
+    errno = EBADF;
+    return -1;
+}
+
