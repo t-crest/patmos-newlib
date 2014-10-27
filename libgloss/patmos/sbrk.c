ba6624bcb361ea98c574aee27085eaecdf159951
Alexander Jordan <ajordan@complang.tuwien.ac.at>
2014-10-27 19:05:33 +0100

    make sure the naked _sbrk is safe

--- libgloss/patmos/sbrk.c
+++ libgloss/patmos/sbrk.c
@@ -24,7 +24,7 @@ extern char __heap_start, __heap_end;
 
 //******************************************************************************
 /// _sbrk - get additional memory.
-void *_sbrk(int nbytes) __attribute__((naked,used));
+void *_sbrk(int nbytes) __attribute__((naked,used,patmos_preserve_tmp));
 void *_sbrk(int nbytes)
 {
   static char *heap_ptr = &__heap_start;
